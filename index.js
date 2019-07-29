0697dc9b40a8a6f9cfb978848549744b0afeb968
pamccombs <pa.mccombs1@gmail.com>
2019-07-29 04:37:15 -0400

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function deepestChild() {
   let node = document.getElementById('grand-node')
   let nextNode = node.children[0]
 
-  while ( nextNode ) {
+  while (nextNode) {
     node = nextNode
     nextNode = node.children[0]
   }
